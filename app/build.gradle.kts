e65981bb392bd960c9ce187ea6ab7a40949f94ae
Ahsan Ahmed <aahsanaahmed26@gmail.com>
2023-10-15 17:32:07 +0500

    Upload/Retrive Images From FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/uploadimgeonfirebase/MainActivity.kt
+++ app/src/main/java/com/example/uploadimgeonfirebase/MainActivity.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.util.UUID
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var choose_img: Button
     lateinit var upload_img: Button
+    lateinit var retrive_img: Button
     lateinit var image_view: ImageView
     var fileUri: Uri? = null
 
@@ -28,6 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         choose_img = findViewById(R.id.choose_image)
         upload_img = findViewById(R.id.upload_image)
+        retrive_img = findViewById(R.id.retrive_image)
+
         image_view = findViewById(R.id.image_view)
 
         choose_img.setOnClickListener {
@@ -35,51 +39,85 @@ class MainActivity : AppCompatActivity() {
             intent.type = "image/*"
             intent.action = Intent.ACTION_GET_CONTENT
             startActivityForResult(
-                Intent.createChooser(intent,"Choose Image to Upload"),0
+                Intent.createChooser(intent, "Choose Image to Upload"), 0
             )
         }
 
         upload_img.setOnClickListener {
-            if (fileUri != null){
+            if (fileUri != null) {
                 uploadImage()
-            }else{
-                Toast.makeText(applicationContext,"Please Select Image to Upload",
-                    Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(
+                    applicationContext, "Please Select Image to Upload",
+                    Toast.LENGTH_LONG
+                ).show()
             }
+        }
+
+        retrive_img.setOnClickListener {
+            retrive_image()
         }
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 0 && resultCode == RESULT_OK && data != null && data.data != null){
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null && data.data != null) {
             fileUri = data.data
             try {
-                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver,fileUri)
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, fileUri)
                 image_view.setImageBitmap(bitmap)
 
-            }catch (e: Exception){
-                Log.e("Exception","Error: "+e)
+            } catch (e: Exception) {
+                Log.e("Exception", "Error: " + e)
             }
         }
     }
 
-    fun uploadImage(){
-        if (fileUri!= null){
+    fun uploadImage() {
+        if (fileUri != null) {
             val progressDialog = ProgressDialog(this)
             progressDialog.setTitle("Uploading Image...")
             progressDialog.setMessage("Processing...")
             progressDialog.show()
 
             val ref: StorageReference = FirebaseStorage.getInstance().getReference()
-                .child(UUID.randomUUID().toString())
+                .child("images")
             ref.putFile(fileUri!!).addOnSuccessListener {
                 progressDialog.dismiss()
-                Toast.makeText(applicationContext,"File Uploaded Successfully", Toast.LENGTH_LONG).show()
-            }.addOnFailureListener{
+                Toast.makeText(applicationContext, "File Uploaded Successfully", Toast.LENGTH_LONG)
+                    .show()
+            }.addOnFailureListener {
                 progressDialog.dismiss()
-                Toast.makeText(applicationContext,"File Upload Failed...", Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, "File Upload Failed...", Toast.LENGTH_LONG)
+                    .show()
             }
         }
     }
+
+    fun retrive_image() {
+        val storageReference: StorageReference = FirebaseStorage.getInstance().reference
+        val image_refrance: StorageReference = storageReference.child("images")
+
+        val progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Retriving Image...")
+        progressDialog.setMessage("Processing...")
+        progressDialog.show()
+
+        image_refrance.downloadUrl.addOnSuccessListener { uri: Uri ->
+
+            Glide.with(this@MainActivity)
+                .load(uri)
+                .into(image_view)
+
+            progressDialog.dismiss()
+            Toast.makeText(this,"Image Retrived Successfull",Toast.LENGTH_LONG).show()
+        }
+            .addOnFailureListener { exception ->
+                progressDialog.dismiss()
+                Toast.makeText(this,"Image Retrived Failed: "+exception.message,Toast.LENGTH_LONG).show()
+
+            }
+    }
+
 }
